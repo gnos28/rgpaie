4640ecdd2cb3daaf477ca4634b7af08d3cc2bc20
gnos28 <gnos28@gmail.com>
2022-08-01 16:41:13 +0200

    database creation

--- database.sql
+++ database.sql
@@ -14,6 +14,176 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cabinet`
+--
+
+DROP TABLE IF EXISTS `cabinet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cabinet` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cabinet`
+--
+
+LOCK TABLES `cabinet` WRITE;
+/*!40000 ALTER TABLE `cabinet` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cabinet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `client` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  `contact` varchar(255) DEFAULT NULL,
+  `mail` varchar(255) DEFAULT NULL,
+  `tel` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES (1,'toto','???','toto@gmail.com','0123456789');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `collaborateur`
+--
+
+DROP TABLE IF EXISTS `collaborateur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `collaborateur` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  `id_cabinet` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_cabinet` (`id_cabinet`),
+  CONSTRAINT `collaborateur_ibfk_1` FOREIGN KEY (`id_cabinet`) REFERENCES `cabinet` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `collaborateur`
+--
+
+LOCK TABLES `collaborateur` WRITE;
+/*!40000 ALTER TABLE `collaborateur` DISABLE KEYS */;
+/*!40000 ALTER TABLE `collaborateur` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dossier`
+--
+
+DROP TABLE IF EXISTS `dossier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dossier` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_planification` int NOT NULL,
+  `id_collaborateur` int NOT NULL,
+  `id_client` int NOT NULL,
+  `code_dossier` varchar(255) DEFAULT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_planification` (`id_planification`),
+  KEY `id_collaborateur` (`id_collaborateur`),
+  KEY `id_client` (`id_client`),
+  CONSTRAINT `dossier_ibfk_1` FOREIGN KEY (`id_planification`) REFERENCES `planification` (`id`),
+  CONSTRAINT `dossier_ibfk_2` FOREIGN KEY (`id_collaborateur`) REFERENCES `collaborateur` (`id`),
+  CONSTRAINT `dossier_ibfk_3` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dossier`
+--
+
+LOCK TABLES `dossier` WRITE;
+/*!40000 ALTER TABLE `dossier` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dossier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `planification`
+--
+
+DROP TABLE IF EXISTS `planification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `planification` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  `date_demande_EVP` int DEFAULT NULL,
+  `date_validation_EVP` int DEFAULT NULL,
+  `desembrouillons` int DEFAULT NULL,
+  `date_paiement` int DEFAULT NULL,
+  `date_envoi_BS_originaux` int DEFAULT NULL,
+  `date_DSN` int DEFAULT NULL,
+  `date_EPP` int DEFAULT NULL,
+  `date_facture` int DEFAULT NULL,
+  `date_VRT` int DEFAULT NULL,
+  `montant_charges` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `planification`
+--
+
+LOCK TABLES `planification` WRITE;
+/*!40000 ALTER TABLE `planification` DISABLE KEYS */;
+/*!40000 ALTER TABLE `planification` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tache`
+--
+
+DROP TABLE IF EXISTS `tache`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tache` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_dossier` int NOT NULL,
+  `nom_tache` varchar(255) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `done` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_dossier` (`id_dossier`),
+  CONSTRAINT `tache_ibfk_1` FOREIGN KEY (`id_dossier`) REFERENCES `dossier` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tache`
+--
+
+LOCK TABLES `tache` WRITE;
+/*!40000 ALTER TABLE `tache` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tache` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +194,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-08-01 15:37:37
+-- Dump completed on 2022-08-01 16:40:24
